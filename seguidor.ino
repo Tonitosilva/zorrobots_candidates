9f1eff98b59321144e4bb39e835e795d9926e254
Tonitosilva <antonio.coronado.ch@gmail.com>
2025-10-17 22:39:22 -0600

    Acctualice el codigo, agregue la parte que controla los sensores de la linea

--- seguidor.ino
+++ seguidor.ino
@@ -1,89 +1,53 @@
-// --- Pines de sensores ---
-#define sensorIzq 2
-#define sensorDer 3
+// Pines de sensores
+#define SENSOR_IZQ A0
+#define SENSOR_DER A1
 
-// --- Pines del puente H ---
-#define ENA 5
-#define IN1 8
-#define IN2 9
-#define IN3 10
-#define IN4 11
-#define ENB 6
+// Pines de motor
+#define ENA 5    // Velocidad motor izquierdo (PWM)
+#define IN1 13   // Cambiado de 4 → 13
+#define IN2 12   // Cambiado de 3 → 12
+#define ENB 6    // Velocidad motor derecho (PWM)
+#define IN3 11   // Cambiado de 7 → 11
+#define IN4 10   // Cambiado de 8 → 10
+
+int valorIzq, valorDer;
+float Kp = 0.05;
+int velocidadBase = 120;
 
 void setup() {
-  pinMode(sensorIzq, INPUT);
-  pinMode(sensorDer, INPUT);
-  
+  pinMode(SENSOR_IZQ, INPUT);
+  pinMode(SENSOR_DER, INPUT);
   pinMode(ENA, OUTPUT);
   pinMode(IN1, OUTPUT);
   pinMode(IN2, OUTPUT);
+  pinMode(ENB, OUTPUT);
   pinMode(IN3, OUTPUT);
   pinMode(IN4, OUTPUT);
-  pinMode(ENB, OUTPUT);
-
   Serial.begin(9600);
 }
 
 void loop() {
-  int izq = digitalRead(sensorIzq);
-  int der = digitalRead(sensorDer);
+  valorIzq = analogRead(SENSOR_IZQ);
+  valorDer = analogRead(SENSOR_DER);
 
-  Serial.print("Izq: ");
-  Serial.print(izq);
-  Serial.print("  Der: ");
-  Serial.println(der);
+  int error = valorIzq - valorDer;
+  int ajuste = Kp * error;
 
-  // Ambos sensores sobre línea blanca → avanzar
-  if (izq == 1 && der == 1) {
-    adelante();
-  }
-  // Sensor izquierdo detecta negro → girar a la izquierda
-  else if (izq == 0 && der == 1) {
-    izquierda();
-  }
-  // Sensor derecho detecta negro → girar a la derecha
-  else if (izq == 1 && der == 0) {
-    derecha();
-  }
-  // Ambos detectan negro → detenerse
-  else {
-    detener();
-  }
+  int velocidadIzq = velocidadBase - ajuste;
+  int velocidadDer = velocidadBase + ajuste;
+
+  velocidadIzq = constrain(velocidadIzq, 0, 255);
+  velocidadDer = constrain(velocidadDer, 0, 255);
+
+  moverAdelante(velocidadIzq, velocidadDer);
 }
 
-// --- FUNCIONES DE MOVIMIENTO ---
-void adelante() {
+void moverAdelante(int velIzq, int velDer) {
   digitalWrite(IN1, HIGH);
   digitalWrite(IN2, LOW);
+  analogWrite(ENA, velIzq);
+
   digitalWrite(IN3, HIGH);
   digitalWrite(IN4, LOW);
-  analogWrite(ENA, 200);
-  analogWrite(ENB, 200);
-}
-
-void izquierda() {
-  digitalWrite(IN1, LOW);
-  digitalWrite(IN2, HIGH);
-  digitalWrite(IN3, HIGH);
-  digitalWrite(IN4, LOW);
-  analogWrite(ENA, 150);
-  analogWrite(ENB, 150);
-}
-
-void derecha() {
-  digitalWrite(IN1, HIGH);
-  digitalWrite(IN2, LOW);
-  digitalWrite(IN3, LOW);
-  digitalWrite(IN4, HIGH);
-  analogWrite(ENA, 150);
-  analogWrite(ENB, 150);
-}
-
-void detener() {
-  digitalWrite(IN1, LOW);
-  digitalWrite(IN2, LOW);
-  digitalWrite(IN3, LOW);
-  digitalWrite(IN4, LOW);
-  analogWrite(ENA, 0);
-  analogWrite(ENB, 0);
+  analogWrite(ENB, velDer);
 }
